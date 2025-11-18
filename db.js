78537089840f35341a2ec4dceecca1a1b7784a81
Mike Zuckerberg <mikezuckerberg2003@gmail.com>
2025-11-18 16:04:38 +0200

    final edit

--- db.js
+++ db.js
@@ -1,20 +1,27 @@
+// db.js
 import mysql from "mysql2";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: false
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("MySQL Connected");
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("MySQL connection error:", err);
+  } else {
+    console.log("MySQL pool connected");
+    connection.release();
+  }
 });
 
-export default db;
+export default pool;
+
